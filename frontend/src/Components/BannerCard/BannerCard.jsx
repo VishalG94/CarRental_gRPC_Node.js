ce0d52fdaa0b2874513b77c9aaec78f04b9dd5a9
cvamsik <55032250+cvamsik@users.noreply.github.com>
2020-04-01 01:51:49 -0700

    banner skeleton done

---
+++ frontend/src/Components/BannerCard/BannerCard.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./BannerCard-styles.css";
+
+const BannerCard = ({ imageURL, text }) => {
+  return (
+    <div className="bannerCard">
+      <img alt="cardimage" src={imageURL} className="cardImage"></img>
+      <p className="cardText">{text}</p>
+    </div>
+  );
+};
+
+export default BannerCard;
--- frontend/src/Components/HomePage/HomePage.jsx
+++ frontend/src/Components/HomePage/HomePage.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import MapDisplay from "../Gmaps/MapDisplay";
+import BannerCard from "../BannerCard/BannerCard";
+import Banner from "../Banner/Banner";
 
 class HomePage extends Component {
   state = {};
@@ -7,7 +9,12 @@ class HomePage extends Component {
     return (
       <div>
         <h1>homepage</h1>
-        <MapDisplay style={{ height: "50vh", width: "50vh" }} />
+        {/* <MapDisplay style={{ height: "50vh", width: "50vh" }} /> */}
+        <BannerCard
+          imageURL="https://media2.zipcar.com/drupal-presales/column-section/ic-saving_0.svg"
+          bottomtext="Zipcar covers gas, insurance, parking, and maintenance for a potential savings of $600/month over car ownership."
+        />
+        <Banner />
       </div>
     );
   }
